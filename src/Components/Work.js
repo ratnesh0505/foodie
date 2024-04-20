b8619894e6193e26b4d6c8c68d5eca7ad366e788
ratnesh0505 <kumarratnesh2002@gmail.com>
2024-04-20 16:22:42 +0530

    first commiit

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -24,7 +24,7 @@ function Work() {
   return (
     <div className='work-section-wrapper'>
         <div className='work-section-top'>
-            <p className='primary-subheading'>Work</p>
+            <p className='primary-subheading'>Work</p>  
             <h1 className='primary-heading'>How It Work</h1>
             <p className='primary-text'>
             we understand the importance of good service. Our friendly staff is here to ensure you have a pleasant dining experience from start to finish.
